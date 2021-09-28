2b57e4fcf6bae89a42e566b6fe971c37ec3671c0
TemTee <temmyteeliz@gmail.com>
2021-09-28 17:28:06 +0100

    nearest point funct

--- Function.R
+++ Function.R
@@ -1,14 +1,48 @@
 library(tidyverse)
 options(digits = 2)
+library(lubridate)
+library(dplyr)
+library(RANN)
+library(readr)
 
-#===================     This Function sums the number of NAs in a DataFrame of same length   
+
+
+# Generates the list of N nearest point -----------------------------------
+
+coord <-  as.data.frame(read_csv( "C:/Users/user/Documents/Udacity/AB Testing/round-roaster-stores.csv") %>% 
+                          select(StoreID,Longitude, Latitude))
+head(coord)
+N_point <- nn2(coord, k=2, treetype = "bd") 
+N_point <- data.frame(nearest_point$nn.idx)  # convert to dataframe
+names(N_point) <- c("Store","1st_Nearest")     #  rename columns
+head(N_point)
+N_point_store <- data.frame(coord$StoreID[n_inx$Store],
+                            coord$StoreID[n_inx$`1st_Nearest`])    # convert index to store id
+names(N_point_store) <- c("Store", "1st_Closest")    # rename columns
+head(N_point_store)
+
+
+
+
+
+# Greetin Function --------------------------------------------------------
+
+
+
+
+
+
+
+
+# ----------  Sum the number of NAs in a DataFrame of same length --------
+
 
 sum_na <- function(df) {
   colSums(is.na(df))
   
 }
 
-# ==========  Check
+# =====  function Check
 
 ab <- data.frame(a = seq(1,62,2),
                  b = rnorm(31),
@@ -17,3 +51,5 @@ ab$a[(seq(5,25,5))] <- NA           #   Replaces values with NA
 ab$b[seq(2,26,2)] <- NA             #   Replaces values with NA
 ab$c[1:4] <- NA                     #   Replaces values with NA
 sum_na(ab)
+
+
